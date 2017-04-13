a9918bd7c164c0a0aef9698f859fbed827c10417
Yash Krishan Verma <yash96iitindore@gmail.com>
2017-04-13 20:27:06 +0530

    Android files added

---
+++ app/src/main/java/com/example/yashkv/wifiname/WiFiName.java
@@ -0,0 +1,13 @@
+package com.example.yashkv.wifiname;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WiFiName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wi_fi_name);
+    }
+}
