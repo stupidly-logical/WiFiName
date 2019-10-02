ba63aa0c73a5024ad41bbcf63fff7cfe667c7aa7
Yash Krishan Verma <yash.krishan.verma@gmail.com>
2019-10-02 19:47:05 +0530

    Gradle updates

--- app/src/main/java/com/example/yashkv/wifiname/WiFiName.java
+++ app/src/main/java/com/example/yashkv/wifiname/WiFiName.java
@@ -1,20 +1,9 @@
 package com.example.yashkv.wifiname;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
-import android.util.Log;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class WiFiName extends AppCompatActivity {
 
